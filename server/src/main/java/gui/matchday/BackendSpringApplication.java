6e6e7ddba800bbf4399ab8a2266413ef3676da5e
Guilherme Santos <gh.vieirasantos@gmail.com>
2024-06-29 18:26:54 -0300

    add spring database application propertiese

--- server/src/main/java/gui/matchday/BackendSpringApplication.java
+++ server/src/main/java/gui/matchday/BackendSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatchdayApplication {
+public class BackendSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MatchdayApplication.class, args);
+		SpringApplication.run(BackendSpringApplication.class, args);
 	}
 
 }
