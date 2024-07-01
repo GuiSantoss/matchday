862538aaaa83cd9f7f9b7d45a4339349aa638be6
Guilherme Santos <gh.vieirasantos@gmail.com>
2024-07-01 13:03:09 -0300

    add TeamResponseDTO

---
+++ server/src/main/java/gui/matchday/dto/team/TeamResponseDTO.java
@@ -0,0 +1,28 @@
+package gui.matchday.dto.team;
+
+import gui.matchday.model.Team;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamResponseDTO {
+
+    private Long id;
+    private String name;
+    private String state;
+    private String photoUrl;
+
+    public TeamResponseDTO(Team team){
+       this(team.getId(), team.getName(), team.getState(), team.getPhotoUrl());
+    }
+}
+
